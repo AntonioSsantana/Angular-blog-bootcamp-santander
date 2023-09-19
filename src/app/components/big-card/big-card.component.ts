3c49abd89a5e156cf7606a55ebbcd546205284f8
Antônio Santana <antonionetosg6@gmail.com>
2023-09-19 13:09:48 -0300

    feat: big-card dinâmico

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+// Mock
+import objMock from '../../../mock/obj.json';
+
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  imageCover: string = '';
+  cardTitle: string = '';
+  cardDescription: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const blog = objMock.data.filter((i) => i.isBig === true);
+  
+    this.imageCover = blog[0].img_url;
+    this.cardTitle = blog[0].title;
+    this.cardDescription = blog[0].description;
   }
-
 }
