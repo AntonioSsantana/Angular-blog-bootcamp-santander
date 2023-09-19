6b464c54ecd7aadb36861fe8fc3414f0855b1a6e
Antônio Santana <antonionetosg6@gmail.com>
2023-09-19 15:07:17 -0300

    feat: criação ddos elementos da página content e renderização do componente header em ambas as páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
 import { HomeModule } from './pages/home/home.module';
-import { ContentComponent } from './pages/content/content.component';
+
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     HomeComponent,
     ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,14 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SmallCardComponent } from 'src/app/components/small-card/small-card.component';
 import { BigCardComponent } from 'src/app/components/big-card/big-card.component';
-import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
-  declarations: [SmallCardComponent, BigCardComponent, HeaderComponent],
+  declarations: [SmallCardComponent, BigCardComponent],
   imports: [
     CommonModule
   ],
-  exports: [SmallCardComponent, BigCardComponent, HeaderComponent],
+  exports: [SmallCardComponent, BigCardComponent],
   providers: [],
   bootstrap: [HomeComponent]
 })
